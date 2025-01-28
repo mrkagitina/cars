c17eefc5f189cb5b3ad32d44353b4394fb6ae6e4
mrkagitina <mrkagitina@yandex.com>
2025-01-28 10:21:42 +0300

    Update CarDaoImpl.java

--- src/main/java/web/dao/CarDaoImpl.java
+++ src/main/java/web/dao/CarDaoImpl.java
@@ -4,7 +4,9 @@ import model.Car;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.stereotype.Component;
 
+@Component
 public class CarDaoImpl implements CarDao {
     private List<Car> cars;
 
